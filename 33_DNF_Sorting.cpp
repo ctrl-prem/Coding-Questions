d0deffc310d7f97167fb52ceadd4944e6f5f9b6e
Yashwant <prem9810683167@gmail.com>
2023-07-17 01:48:55 +0530

    DNF Sort

--- 33_DNF_Sorting.cpp
+++ 33_DNF_Sorting.cpp
@@ -1,5 +1,11 @@
 // DNF_Sorting is used to sort an array having 3 known values only.
 
+// It is used to sort container with three different elements(can be integer or any).
+// introducing first, mid, and last positions, 
+// where first shows area for 'first type of element' ,
+// mid shows 'second type of element' or 'area of unknown element at same time' and 
+// last shows 'third type of element'.
+
 #include<iostream>
 using namespace std;
 
@@ -10,23 +16,22 @@ int main(){
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-    int start=-1, mid=0, last=n;
+    int first=-1, mid=0, last=n;
     for(; mid<last; ){
         if(arr[mid]==0){
-            swap(arr[start+1], arr[mid]);
-            start++;
+            swap(arr[++first], arr[mid]);
             mid++;
         }
         else if(arr[mid]==1){
             mid++;
         }
         else{
-            swap(arr[last-1], arr[mid]);
-            last--;
+            swap(arr[--last], arr[mid]);
         }
     }
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
     return 0;
 }
