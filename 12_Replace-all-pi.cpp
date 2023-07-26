ad2e07a8e16a80531888d87453bcec108e1fb272
Yashwant <prem9810683167@gmail.com>
2023-07-27 01:05:08 +0530

    recursion

--- 12_Replace-all-pi.cpp
+++ 12_Replace-all-pi.cpp
@@ -1,9 +1,36 @@
 // replace all the "pi" with "3.14" in the given string.
 // make the binary/recursion tree of the solution.
 #include<iostream>
+#include<string>
 using namespace std;
 
+void replacepi(string& str, int i){
+    // base case
+
+
+    // recursive case
+    if(str[i]=='p' and str[i+1]=='i'){
+        int j = i+2;
+        while(j!='\0') j++;
+        while(j>=i+2){
+            str[j+2] = str[j];
+            j--;
+        }
+        str[i] = '3';
+        str[i+1] = '.';
+        str[i+2] = '1';
+        str[i+3] = '4';
+        replacepi(str, i+4);
+    }
+    else{
+        replacepi(str, i+1);
+    }
+}
+
 int main(){
-    ...
+    string str;
+    cin>>str;
+    replacepi(str, 0);
+    cout<<str<<endl;
     return 0;
 }
--- 30_generate_subsequeces.cpp
+++ 30_generate_subsequeces.cpp
@@ -4,12 +4,53 @@
 #include <vector>
 using namespace std;
 
+// Iterative Approach
+void subsequences(vector<int> v, int n){
+    int a = pow(2, n); // total no. of subsequences possible.
+    int cnt = 0;
+    for(int i=0; i<a; i++){
+        vector<int> out;
+        for(int j=0; j<n; j++){
+            if((i>>j)&1) out.push_back(v[j]);
+        }
+        cnt++;
+        for(int k=0; k<out.size(); k++){
+            cout<<out[k]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<cnt<<endl;
+}
+
+// Recursive Approach
+void rec_subseq(vector<int> v, vector<int> out, int i){
+    // base case
+    if(i>=v.size()){
+        for(int j=0; j<out.size(); j++){
+            cout<<out[j]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+
+    // recursive case
+    out.push_back(v[i]);
+    rec_subseq(v, out, i+1);
+    out.pop_back();
+    rec_subseq(v, out, i+1);
+}
+
 int main(){
-    vector<int> v;
+    
     int n;
     cin>>n;
+    vector<int> v(n);
     for(int i=0; i<n; i++){
-        
+        cin>>v[i];
     }
+    // subsequences(v, n);
+    // cout<<endl;
+    vector<int> out;
+    rec_subseq(v, out, 0);
     return 0;
 }
