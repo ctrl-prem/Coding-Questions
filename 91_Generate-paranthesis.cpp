03d161dffc99de53868247ff6c4cc558e446e75d
Yashwant <prem9810683167@gmail.com>
2023-08-08 11:11:17 +0530

    recursion

--- 91_Generate-paranthesis.cpp
+++ 91_Generate-paranthesis.cpp
@@ -34,15 +34,39 @@ vector<string> solution(int n){
     return ans;
 }
 
+// Using string and recursion.
+void solve(int n, int x, int y, string out){
+    // base case
+    if(x==n and y==n){
+        cout<<out<<endl;
+        return;
+    }
+    // recursive case
+    if(x<n){
+        out += "(";
+        solve(n, x+1, y, out);
+        out.pop_back(); //here pop is need as while returning back from recursion x become 1 but out = "((" instead of "(".
+        // cout<<x<<"_"<<out<<endl;
+    }
+    if(y<n){
+        out += ")";
+        solve(n, x, y+1, out);
+        // out.pop_back(); // not compulsory as it is taken care by the recursion itself(the poping of character).
+        // cout<<"_"<<out<<"_"<<y;
+    }
+}
+
 int main(){
     int n;
     cin>>n;
-    vector<string> vect;
+    // vector<string> vect;
     // char out[20];
-    vect = solution(n);
-    for(int i=0; i<vect.size(); i++){
-        cout<<vect[i]<<endl;
-    }
+    // vect = solution(n);
+    // for(int i=0; i<vect.size(); i++){
+    //     cout<<vect[i]<<endl;
+    // }
+    string out = "";
+    solve(n, 0, 0, out);
     // cout<<endl;
     return 0;
 }
--- rough.cpp
+++ rough.cpp
@@ -2,7 +2,65 @@
 #include<vector>
 using namespace std;
 
+vector<int> solve(vector<int>& A, vector<int>& B){
+    vector<int> ans;
+    int n = B.size();
+    int sum_A = 0;
+    for(int i=0; i<n; i++){
+        sum_A += A[i];
+    }
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        int sum = 0;
+        int x = B[i];
+        cnt = (x/sum_A)*n;
+        sum = cnt*sum_A;
+
+    }
+    for(int i=0; i<B.size(); i++){
+        int x = B[i];
+        int j=0;
+        int sum = 0;
+        int count = 0;
+        while(true){
+            sum+=A[j];
+            count++;
+            if(sum >= x){
+                ans.push_back(count);
+                break;
+            }
+            if(j == n-1){
+                if(sum<=0){
+                    ans.push_back(-1);
+                    break;
+                }
+                j = 0;
+                continue;
+            }
+            j++;
+        }
+    }
+    return ans;
+}
+
 int main(){
-    vector<int> v = {1,2,3,4,5};
-    sample ="prem is hero";
+    // vector<int> A;
+    // for(int i=0; i<3; i++){
+    //     int x;
+    //     cin>>x;
+    //     A.push_back(x);
+    // }
+    // vector<int> B;
+    // for(int i=0; i<3; i++){
+    //     int x;
+    //     cin>>x;
+    //     B.push_back(x);
+    // }
+    // vector<int> answer = solve(A, B);
+    // for(int i=0; i<answer.size(); i++){
+    //     cout<<answer[i]<<" ";
+    // }
+    // cout<<endl;
+    // return 0;
+    cout<<(20%3==0)<<endl;
 }
