b0e9ea35fb63c8f19b4b0f4b0798b3fd2c73b760
Yashwant <prem9810683167@gmail.com>
2023-08-20 21:33:50 +0530

    dp

--- 98_Ladders-Problem.cpp
+++ 98_Ladders-Problem.cpp
@@ -4,6 +4,8 @@
 #include<vector>
 using namespace std;
 
+
+// printing the path using recursion. and returning the number of ways.
 vector<int> help;
 
 int solve1(int n, int i, int k){
@@ -26,6 +28,7 @@ int solve1(int n, int i, int k){
     return count;
 }
 
+// returing the no. of ways, without printing the path using recursion.
 int solve(int n, int k, int i){
     if(i == n) return 1;
 
@@ -39,12 +42,36 @@ int solve(int n, int k, int i){
     return count;
 }
 
+// Using dp, returning the no. of ways, without printing the path using recursion.
+int solveDp(int n, int k, int i, vector<int>& dp){
+    // Look up
+    if(dp[i] != -1) return dp[i];
+
+    // Base case
+    if(i == n) return dp[i] = 1;
+
+    // Recursive case
+    int count = 0;
+    for(int j=1; j<=k; j++){
+        if(i+j <= n){
+            count += solveDp(n, k, i+j, dp);
+        }
+        else break;
+    }
+    return dp[i] = count;
+}
+
 int main(){
     int n; 
     cin>>n;
     int k;
     cin>>k;
     cout<<solve(n, k, 0)<<endl;
+
     cout<<solve1(n, 0, k)<<endl;
+
+    vector<int> dp(n+1, -1);
+    cout<<solveDp(n, k, 0, dp)<<endl;
+
     return 0;
 }
