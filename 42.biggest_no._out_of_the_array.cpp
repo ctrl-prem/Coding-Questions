5a7c59c56dec61a408408be21ca76e4ed7c23f46
Yashwant <prem9810683167@gmail.com>
2023-08-25 13:04:59 +0530

    Sort func Update

--- 42.biggest_no._out_of_the_array.cpp
+++ 42.biggest_no._out_of_the_array.cpp
@@ -3,7 +3,13 @@
 #include<cstring>
 using namespace std;
 
-bool sortforbiggest(string s1, string s2){
+bool sortforbiggest(string s1, string s2){ // ex: str[n] = {"54", "546", "548", "60"}. here at every point we take two strings and compare them. if s1+s2>s2+s1 then we return true else false. for example first we take s1 = "54" and s2 = "546" and compared that "54546" > "54654" which is true so we return true. then we take s1 = "54" and s2 = "548" and compared that "54548" > "54854" which is true so we return true. then we take s1 = "54" and s2 = "60" and compared that "5460" > "6054" which is false so we return false. then we take s1 = "546" and s2 = "548" and compared that "546548" > "548546" which is false so we return false. then we take s1 = "546" and s2 = "60" and compared that "54660" > "60546" which is false so we return false. then we take s1 = "548" and s2 = "60" and compared that "54860" > "60548" which is false so we return false. so the final array will be {"60", "548", "546", "54"}. It completed in O(nlogn) time complexity. Explaination for the time complexity is given below.
+// Time Complexity: O(nlogn) because we are using sort function which takes O(nlogn) time complexity.
+// And the sort function uses quick sort algorithm which takes O(nlogn) time complexity.
+// And the quick sort algorithm uses divide and conquer method which takes O(nlogn) time complexity.
+// And the divide and conquer method uses recursion which takes O(nlogn) time complexity.
+// And the recursion uses stack which takes O(nlogn) time complexity.
+
     if(s1+s2>s2+s1){
         return true;
     }
