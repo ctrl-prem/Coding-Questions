e4747119c734bb9286c0447297474bc3961c7579
Yashwant <prem9810683167@gmail.com>
2023-08-27 23:43:11 +0530

    .

--- 50_Generate_Permutations.cpp
+++ 50_Generate_Permutations.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
+#include<string>
+#include<vector>
 using namespace std;
 
+void solution(string s, vector<string> &out, int i){
+    // Base Case
+    if(i==s.length()){
+        out.push_back(s);
+        return;
+    }
+
+    // Recursive Case
+    for(int j=i; j<s.length(); j++){
+        swap(s[i], s[j]);
+        solution(s, out, i+1);
+        swap(s[i], s[j]); // Backtracking step to restore the original string for next iteration of loop.
+    }
+}
+
 int main(){
-    ...
+    string s;
+    cin>>s;
+    vector<string> out;
+    solution(s, out, 0);
+    for(string i : out){
+        cout<<i<<" ";
+    }
+    cout<<endl;
     return 0;
 }
--- 51_Generate_Unique_Permutations.cpp
+++ 51_Generate_Unique_Permutations.cpp
@@ -1,9 +1,62 @@
 #include<iostream>
+#include<set>
+#include<string>
 using namespace std;
 // char array with repetitions.
 // method 1:- Using set(it only contain unique char array(string)).
 // method 1:- is time consuming as we are still generating duplicate permutations.
+
+void solve(string s, set<string>& out, int i){
+    if(i==s.length()){
+        out.insert(s);
+        return;
+    }
+
+    for(int j=i; j<s.length(); j++){
+        swap(s[i], s[j]);
+        solve(s, out, i+1);
+        swap(s[i], s[j]);
+    }
+    return;
+}
+
+void solution(string s, vector<string> &out, int i){
+    if(i==s.length()){
+        out.push_back(s);
+        return;
+    }
+
+    for(int j=i; j<s.length(); j++){
+        bool flag = true;
+        for(int k=j+1; k<s.length(); k++){
+            if(s[k]==s[j]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            swap(s[i], s[j]);
+            solution(s, out, i+1);
+            swap(s[i], s[j]);
+        }
+    }
+    return;
+}
+
 int main(){
-    ...
+    string s;
+    cin>>s;
+    set<string> out;
+    vector<string> out1;
+    solve(s, out, 0);
+    solution(s, out1, 0);
+    for(auto i : out){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    for(auto i : out1){
+        cout<<i<<" ";
+    }
+    cout<<endl;
     return 0;
 }
