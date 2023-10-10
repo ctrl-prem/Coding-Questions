6a93193e2461a5bf653e837c1d8d7950420a14fe
Yashwant <prem9810683167@gmail.com>
2023-10-10 20:25:12 +0530

    LL

--- 27_Detect-cycle@LL.cpp
+++ 27_Detect-cycle@LL.cpp
@@ -1,7 +1,107 @@
-#include<bits/stdc++.h>
+// detect node at which cycle is starting and print the value.
+
+#include<iostream>
 using namespace std;
- 
+
+class ListNode{
+    public:
+        int val;
+        ListNode* next;
+
+        ListNode(int val){
+            this->val = val;
+            this->next = NULL;
+        }
+};
+
+void printNodes(ListNode* head){
+    while(head){
+        cout<<head->val<<" ";
+        head = head->next;
+    }
+}
+
+bool isCyclePresent(ListNode* head){
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while(fast and fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(fast == slow){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+ListNode* detectCyclicNode(ListNode* head){
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while(fast and fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            break;
+        }
+    }
+
+    if(!fast or !fast->next){
+        return NULL;
+    }
+
+    slow = head;
+    while(slow!=fast){
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return slow;
+}
+
+void breakThecycle(ListNode* head){
+    ListNode* prev = NULL;
+    ListNode* slow = head;
+    ListNode* fast = head;
+    
+    while(fast and fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            break;
+        }
+    }
+    if(!fast or !fast->next){
+        return;
+    }
+
+    slow = head;
+    while(slow->next!=fast->next){
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    fast->next = NULL;
+    return;
+}
+
 int main(){
-    ...
+    ListNode* head = new ListNode(10);
+    head->next = new ListNode(20);
+    head->next->next = new ListNode(30);
+    head->next->next->next = new ListNode(40);
+    head->next->next->next->next = new ListNode(50);
+    head->next->next->next->next->next = head->next->next;
+
+    isCyclePresent(head) ? cout<<"Yes Present"<<endl : cout<<"Not Present"<<endl;
+
+    ListNode* cyclicNode = detectCyclicNode(head);
+    cyclicNode ? cout<<cyclicNode->val<<endl : cout<<"NAN"<<endl;
+
+    breakThecycle(head);
+    printNodes(head);
     return 0;
 }
--- 28_check-palindrome@LL.cpp
+++ 28_check-palindrome@LL.cpp
@@ -1,7 +1,76 @@
 #include<iostream>
 using namespace std;
 
+class ListNode{
+    public:
+        int val;
+        ListNode* next;
+
+        ListNode(int val){
+            this->val = val;
+            this->next = NULL;
+        }
+};
+
+void printNodes(ListNode* head){
+    while(head){
+        cout<<head->val<<" ";
+        head = head->next;
+    }
+}
+
+ListNode* midPt(ListNode* head){
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+
+    while(fast and fast->next){
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    return slow;
+}
+
+ListNode* reverse(ListNode* newHead){
+    ListNode* prev = NULL;
+    ListNode* curr = newHead;
+
+    while(curr){
+        ListNode* temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+
+    return prev;
+}
+
+bool palindrome(ListNode* head){
+    ListNode* midPoint = midPt(head);
+    ListNode* newHead = midPoint->next;
+    midPoint->next = NULL;
+    newHead = reverse(newHead);
+
+    while(newHead){
+        if(head->val != newHead->val){
+            return false;
+        }
+
+        head = head->next;
+        newHead = newHead->next;
+    }
+
+    return true;
+}
+
 int main(){
-    ...
+    ListNode* head = new ListNode(10);
+    head->next = new ListNode(20);
+    head->next->next = new ListNode(30);
+    head->next->next->next = new ListNode(20);
+    head->next->next->next->next = new ListNode(20);
+    head->next->next->next->next->next = new ListNode(10);
+
+    cout<<palindrome(head)<<endl;
     return 0;
 }
