601173468847947eb19b78cf24bc0b0b6b18b564
Yashwant <prem9810683167@gmail.com>
2023-10-11 10:56:14 +0530

    LL

--- 29_Merge-two-sorted-LL.cpp
+++ 29_Merge-two-sorted-LL.cpp
@@ -44,6 +44,52 @@ ListNode* merge(ListNode* head1, ListNode* head2){
     return head;
 }
 
+ListNode* iterativeMerge(ListNode* head1, ListNode* head2){
+    if(!head1) return head2;
+    if(!head2) return head1;
+
+    // ListNode* head = NULL;
+    // if(head1->val >= head2->val){
+    //     head = head2;
+    //     head2 = head2->next;
+    //     head->next = NULL;
+    // }
+    // else{
+    //     head = head1;
+    //     head1 = head1->next;
+    //     head->next = NULL;
+    // }
+
+    ListNode* head = new ListNode(0); // Concept of dummy node. solving using dummy node.
+    ListNode* temp = head;
+
+    while(head1 and head2){
+        if(head1->val >= head2->val){
+            temp->next = head2;
+            head2 = head2->next;
+            if(temp->next){
+                temp = temp->next;
+            }
+        }
+        else{
+            temp->next = head1;
+            head1 = head1->next;
+            if(temp->next){
+                temp = temp->next;
+            }
+        }
+    }
+
+    if(head1){
+        temp->next = head1;
+    }
+    if(head2){
+        temp->next = head2;
+    }
+
+    return head->next;
+}
+
 int main(){
     ListNode* head1 = new ListNode(10);
     head1->next = new ListNode(30);
@@ -53,7 +99,10 @@ int main(){
     head2->next = new ListNode(40);
     head2->next->next = new ListNode(60);
 
-    ListNode* head = merge(head1, head2);
-    printNodes(head);
+    // ListNode* head = merge(head1, head2);
+    // printNodes(head);
+
+    ListNode* headi = iterativeMerge(head1, head2);
+    printNodes(headi);
     return 0;
 }
--- 57_k-rotation@LL.cpp
+++ 57_k-rotation@LL.cpp
@@ -1,8 +1,115 @@
-// Cyclic rotation
+// Using cyclic as well as normal method.
 #include<iostream>
 using namespace std;
 
+class ListNode{
+    public:
+        int val;
+        ListNode* next;
+
+        ListNode(int val){
+            this->val = val;
+            this->next = NULL;
+        }
+};
+
+void printNodes(ListNode* head){
+    while(head){
+        cout<<head->val<<" ";
+        head = head->next;
+    }
+}
+
+int lengthofLL(ListNode* head){
+
+    int cnt = 0;
+    while(head){
+        cnt++;
+        head=head->next;
+    }
+
+    return cnt;
+}
+
+ListNode* kRotation(ListNode* head, int k){
+
+    if(!head){
+        return head;
+    }
+
+    int n = lengthofLL(head);
+    k = k%n;
+
+    ListNode* fast = head;
+    ListNode* tail = NULL;
+    for(int i=0; i<k+1; i++){ //(k+1) coz we need sub-List of len. k
+        if(!fast->next){
+            tail = fast;
+        }
+        fast = fast->next;
+    }
+
+    ListNode* slow = head;
+    while(fast){
+        if(fast->next == NULL){
+            tail = fast;
+        }
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    if(!slow->next){
+        return head;
+    }
+
+    ListNode* newHead = slow->next;
+    slow->next = NULL;
+    tail->next = head;
+
+    return newHead;
+}
+
+ListNode* kRotation2(ListNode* head, int k){
+    if(!head) return head;
+
+    int n = 1;
+    ListNode* tail = head;
+    while(tail->next){
+        n++;
+        tail = tail->next;
+    }
+    k = k%n;
+    if(k == 0){
+        return head;
+    }
+    
+    ListNode* newTail = head;
+    for(int i=0; i<n-k-1; i++){
+        newTail = newTail->next;
+    }   
+
+    ListNode* newHead = newTail->next;
+    newTail->next = NULL;
+    tail->next = head;
+
+    return newHead;
+
+}
+
 int main(){
-    ...
+    // ListNode* head = NULL;
+    ListNode* head = new ListNode(10);
+    head->next = new ListNode(20);
+    head->next->next = new ListNode(30);
+    head->next->next->next = new ListNode(40);
+    head->next->next->next->next = new ListNode(50);
+
+    int k;
+    cin>>k;
+    // head = kRotation(head, k);
+    // printNodes(head);
+
+    head = kRotation2(head, k);
+    printNodes(head);
     return 0;
 }
